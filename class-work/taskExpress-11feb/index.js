4b10ef209bc43d24e426fd975049c64c6a4888c1
anxnt-goyl <anantg0609@gmail.com>
2026-02-11 12:56:46 +0530

    11-feb

---
+++ class-work/taskExpress-11feb/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+app.get("/",(req,res)=>{
+    res.send("Hello ji")
+});
+app.get("/home",(req,res)=>{
+    res.send(`<h1>Swagat hai apka home page pr </h1>
+        <a href='/about'>click kariye mere bare mai janne ke liye <a/>`);
+});
+app.get("/about",(req,res)=>{
+    res.send("Swagat hai apka about page pr ")
+});
+app.get("/contact",(req,res)=>{
+    res.send("Swagat hai apka contact page pr ")
+});
+app.get("/search",(req,res)=>{
+    console.log(req.query);
+    res.send("Seach result served " +req.query.name);
+});
+app.use((req,res)=>{
+    res.status(404).send("Page not found"); 
+})
+app.listen(8000); 
