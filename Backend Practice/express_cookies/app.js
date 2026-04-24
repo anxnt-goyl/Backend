1a9cc3d6d062d9c47c49bf71d2842a8293871d48
anxnt-goyl <anantg0609@gmail.com>
2026-04-24 13:31:17 +0530

    23April

--- Backend Practice/express_cookies/app.js
+++ Backend Practice/express_cookies/app.js
@@ -6,10 +6,24 @@ app.use(cookieparser());
 app.get("/setcookie",(req,res)=>{
     res.cookie("name","Secfb",
         {
-            maxAge:24*60*60*1,
+            maxAge:24*60*60*1000,
             httpOnly:true,
             secure:false
         });
     res.send("Cookie is setup");
 });
+
+app.get("/getcookie",(req,res)=>{
+    const data = req.cookies.name;
+    if(data){
+        res.send(data);
+    }
+    else{
+        res.send("First create cookie");
+    } 
+})
+app.get("/deletecookie",(req,res)=>{
+    res.clearCookie("name");
+    res.send("Cookie deleted");
+})
 app.listen(8080);
